017829d7a3f7fff1617c89a5aa8474e772eac8b3
Darklaneanjana <darklaneanjana@gmail.com>
2022-03-22 14:37:34 +0530

    2

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.static("./public"));
 const events = require("./routes/events.js");
@@ -12,29 +15,35 @@ const trainers = require("./routes/trainers.js");
 const auth = require("./routes/auth.js");
 const { connect } = require("./routes/events.js");
 
-// Testing
+//test/////////////////////////////////////////////////////////////////////////////////////////////////
+const jwt = require("jsonwebtoken");
+
+app.get("/t", (req, res) => {
+  // const token = jwt.sign({ _id: 12345, type: "T" }, process.env.TOKEN_SECRET);
+  res.setHeader("set-Cookie", "newUser=true");
+  res.send("dfdf");
+});
+
 const verifyToken = require("./routes/verifyToken");
 app.get("/test", verifyToken, (req, res) => {
   console.log(req.user);
   res.send("test");
 });
-//
+///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 app.use("/api/events", events);
 app.use("/api/trainers", trainers);
 app.use("/api/auth", auth);
 app.use("*", (req, res) => res.status(404).json({ error: "Not Found" }));
 
-const uri = process.env.ATLAS_URI;
-const port = process.env.port || 5000;
-
 mongoose
-  .connect(uri, { useNewUrlParser: true })
+  .connect(process.env.ATLAS_URI, { useNewUrlParser: true })
   .catch((err) => {
     console.error(err.stack);
     process.exit(1);
   })
   .then(async (client) => {
+    const port = process.env.port || 5000;
     app.listen(port, () => {
       console.log(`Server is running on port: ${port}`);
     });
